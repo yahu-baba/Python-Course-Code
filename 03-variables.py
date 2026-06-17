1677a6d1a086c81f6e77bc56691703c5a45180e9
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-06-17 07:39:16 +0530

    Python Variables Video Code

---
+++ 03-variables.py
@@ -0,0 +1,42 @@
+# name = "sanchit"
+# age = 25
+# height = 5.10
+
+# name = "YAHUBABA"
+
+# print(name)
+# print(age)
+# print(height)
+
+# age = "hello"
+# print(age)
+
+# user_name = "john"
+# student1 = "Alex"
+# _marks = 90
+# myName = "john"
+# MyName = "john"
+
+# 2name = "john"
+
+# first_name = "John"
+
+# a, b, c = 10, 20, 30
+# a, b, c = "Orange", "Banana", "Mango"
+
+# print(a)
+# print(b)
+# print(c)
+
+# x = y = z = 100
+
+# print(x)
+# print(y)
+# print(z)
+
+name = "John"
+color = "red"
+age = 25
+
+# print("Your name is " + name + ". Your favorite color is " + color)
+print("Your name is" , name , ". Your Age is" , age)
