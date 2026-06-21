a9e5827b73088f3829e0fc25fa303e5d518dedce
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-06-21 11:30:12 +0530

    List and Tuple Methods

---
+++ 07-List-Methods.py
@@ -0,0 +1,40 @@
+# fruits = ["Cherry","mango","Banana","apple" ]
+veg = ("Potato", "Tomato")
+
+# fruits.append("Mango")
+# fruits.insert(1, "Mango")
+# fruits.insert(0, 45)
+# fruits.remove("banana")
+# fruits.pop(0)
+# fruits.clear()
+# fruits.sort(reverse=True)
+# fruits.sort()
+# fruits.reverse()
+# x = fruits.copy()
+# x = list(fruits)
+# x = fruits[0:2]
+
+# fruits.extend(veg)
+
+fruits = ["cherry","mango","banana","apple","mango" ]
+numbers = [10, 20, 6, 44, 32]
+
+# x = fruits.index("mango", 2, 6)
+
+# x = fruits.count("Grapes")
+# x = len(numbers)
+# x = max(numbers)
+# x = min(numbers)
+# x = min(fruits)
+
+# colors = ("red", "green")
+# colors = "red"
+numbers = {10, 20, 30}
+
+# color_list = list(colors)
+result = list(numbers)
+
+# print(color_list)
+print(result)
+# print(fruits)
+# print("Minimum :", x)
---
+++ 08-Tuple-Method.py
@@ -0,0 +1,27 @@
+# fruits = ("apple", "banana", "apple", "orange", "apple")
+# fruits = "apple", "banana", "apple", "orange", "apple"
+# nested_tuple = ((10,20), (30, 40), (50, 60))
+
+# x = fruits.index("apple", 1, 3)
+# print(x)
+
+# print(nested_tuple[0][1])
+# print(nested_tuple[0][1])
+# print(fruits.count("banana"))
+
+# numbers = (20, 34, 51, 45, 10)
+
+# x = numbers.index(51)
+# x = max(numbers)
+# x = min(numbers)
+# x = len(numbers)
+# print(x)
+
+# numbers = [20, 34, 51, 45, 10]
+# string = "hello"
+# colors = {"red", "green"}
+r = range(5)
+
+t = tuple(r)
+
+print(t)
