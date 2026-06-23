b7f4d788fe7d79619c1206aa7b62db833fe60497
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-06-23 06:40:41 +0530

    Python Dictionary Video Code

---
+++ 09-Dictionary.py
@@ -0,0 +1,47 @@
+student = { 
+   "name" : "John", 
+   "age" : 20,
+   "marks" : 89.45,
+   "passed": True,
+   "skills" : ["Python", "Javascript", "React"]
+}
+
+# if "gender" in student:
+#    print("Key Found")
+
+# for value in student.values():
+#    print(value)
+
+# for key, value in student.items():
+#    print(key," : " , value)
+
+# student = {}
+# student = dict(
+#    name = "John",
+#    age = 20
+# )
+
+# student["gender"] = "male"
+# student["age"] = 25
+
+# print(student)
+# print(student["skills"])
+# print(student["skills"][0])
+
+# print(type(student))
+
+# print(student["name"])
+# print(student["age"])
+
+# students = {
+#    "student1": {
+#       "name" : "Ram",
+#       "age" : 20
+#    },
+#    "student2": {
+#       "name" : "Shyam",
+#       "age" : 22
+#    }
+# }
+
+# print(students["student1"]["name"])
---
+++ 10-Dictionary-Methods.py
@@ -0,0 +1,39 @@
+student = {
+   "name": "Sanchit",
+   "age": 20, 
+   "city": "Delhi"
+}
+
+text = str(student)
+
+print(text)
+print(type(text))
+
+# print(len(student))
+
+# print(student.setdefault("gender", "Unknown"))
+# print(student)
+# student.update({"city": "Delhi"})
+# student.update({"age": 25})
+# student.update(city="Delhi", course= "Python")
+# new_student = student.copy()
+
+
+# print(new_student)
+# age = student.pop("age")
+# print(student.popitem())
+# student.clear()
+# print(age)
+
+# print(student.get("city","Not found"))
+# print(list(student.keys()))
+# print(student.items())
+
+# for key, value in student.items():
+#    print(key, ":", value)
+
+# keys = ["name", "age", "city"]
+
+# data = dict.fromkeys(keys, "Unknown")
+
+# print(data)
