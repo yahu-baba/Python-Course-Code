c207353886b7c89312e0a1af997cfaa4c5cc3e92
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-06-24 08:12:37 +0530

    Python Set video code

---
+++ 11-Set.py
@@ -0,0 +1,28 @@
+fruits = {"apple", "banana","mango"}
+
+new_fruits = fruits.copy()
+print(new_fruits )
+# fruits.add("mango")
+# more_fruits = ["mango", "orange"]
+# fruits.update(more_fruits)
+# fruits.remove("lichi")
+# fruits.discard("mango")
+# item = fruits.pop()
+# print(item)
+# fruits.clear()
+# print(fruits)
+
+# for item in fruits:
+#    print(item)
+
+# print("orange" in fruits)
+# print(type(fruits))
+
+# numbers = {10, 20, 30, 10, 20}
+# print(numbers)
+
+# data = {10, "hello", 3.14, True}
+# print(data)
+
+# data = set()
+# print(type(data))
---
+++ 12-Set-Methods.py
@@ -0,0 +1,39 @@
+A = {1, 2, 3}
+# B = {3, 4, 2}
+
+fruits = {"Orange", "Apple"}
+
+# print(len(fruits))
+
+# print(max(fruits))
+print(min(A))
+
+# print(A ^ B)
+# print(A.symmetric_difference(B))
+
+# print(A - B)
+# print(A.difference(B))
+
+# print(A & B)
+# print(A.intersection(B))
+
+# print(A | B)
+# print(A.union(B))
+
+# A = {1, 2, 3, 4}
+# B = {1, 2}
+
+# print(A.issuperset(B))
+# print(A.issubset(B))
+
+# A = {1, 2}
+# B = {3, 4}
+
+# print(A.isdisjoint(B))
+
+# number = [10, 20, 30, 10, 20, 40]
+
+# unique_number = set(number)
+
+# print(unique_number)
+
