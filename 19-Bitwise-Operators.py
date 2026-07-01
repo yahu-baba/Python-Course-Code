45f0d5f7142def78a23b22f2fa3474fc9d41e9a9
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-07-01 10:24:13 +0530

    Bitwise and Assignment Operators Video code

---
+++ 19-Bitwise-Operators.py
@@ -0,0 +1,19 @@
+# a = 10  # 1010
+# b = 6   # 0110
+
+# print(bin(a ^ b))
+# print(a ^ b)
+
+# print(bin(a))
+# print(bin(b))
+
+# print(bin(~b)) #~x = -(x + 1)
+
+# print(~b)
+
+a = 20
+
+# print(a << 3)
+print(a >> 3)
+
+
