54cc23636579ae17ce6e00010215ee482d1bc7ca
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-07-03 12:47:29 +0530

    If Statement & If Else Video Code

---
+++ 21-If-Statement.py
@@ -0,0 +1,49 @@
+# a = 10
+# b = 100
+
+# if a >= b : 
+#    print("A is Greater")
+#    print("This is just test")
+#    print("This is another test")
+
+# print("End")   
+
+# age = int(input("Enter your Age: "))
+
+# if age >= 18:
+#    print("You are eligible to vote.")
+# else:
+#    print("You are not eligible to vote.")
+
+
+
+# number = int(input("Enter a Number: "))
+
+# if number > 0:
+#    print("The number is positive.")
+# else:
+#    print("The number is negative.")
+       
+# number = [10, 20, 25, 33, 45]
+
+# if 33 in number:
+#    print("Present")
+# else:   
+#    print("Not Present")
+
+# name = input("Enter your Name : ")
+# gender = input("Enter Gender : ")
+
+# if(gender == 'male'):
+#    print("Hello Mr.", name)
+# else:
+#    print("Hello Miss.", name) 
+
+
+username = input("Enter UserName : ")
+password = input("Enter Password : ")
+
+if(username == "admin" and password == "1234"):
+   print("Login Successful")
+else:
+   print("Invalid username or password") 
