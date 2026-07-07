35562fd5f06f4de03cb2961cab72c492d987eec9
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-07-07 09:41:28 +0530

    Python For Loop - video code

---
+++ 26-For-Loop.py
@@ -0,0 +1,59 @@
+# fruits = ["apple", "banana", "orange"]
+# fruits = ("apple", "banana", "orange")
+# fruits = {"apple", "banana", "orange"}
+
+# for fruit in fruits:
+#    print(fruit)   
+
+# name = "Python"
+
+# for letter in name:
+#    print(letter)
+
+# student = {
+#    "name": "Sanchit",
+#    "age": 20,
+#    "course": "Python"
+# }
+
+# for key, value in student.items():
+#    print(key, ":", value)
+
+# fruits = ["apple", "banana", "orange"]
+
+# for index, fruit in enumerate(fruits):
+#    print(index, ":", fruit)  
+
+# name = "Python is Good"
+
+# for index, letter in enumerate(name):
+#    print(index, ":", letter)
+
+# students = ["Rahul", "Amit", "Neha"]
+# marks = [85, 92, 78]
+
+# for name, mark in zip(students, marks):
+#    print(name, "scored", mark)
+
+# for i in range(5):
+#    print(i)
+
+# for i in range(2, 15, 3):
+#    print(i)
+
+# for i in range(10, 0, -2):
+#    print(i)
+
+# for num in range(1 , 11):
+#    if num % 2 != 0:
+#       print(num)
+
+# for num in range(1 , 11):
+#    print(f"2 x {num} = {2 * num}")
+   
+# print("2 x", num, "=", 2 * num)
+
+for num in range(1, 6):
+   print(num)
+else:
+   print("Loop completed.")
