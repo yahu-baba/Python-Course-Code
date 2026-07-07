963b7a017925a542bb96853a6a532ad63d190a2f
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-07-07 10:50:49 +0530

    Python While Loop - video code

---
+++ 27-While-Loop.py
@@ -0,0 +1,50 @@
+# count = 1
+
+# while count <= 10:
+#    print(count)
+#    count += 3
+
+# count = 10
+
+# while count >= 1:
+#    print(count)
+#    count -= 2
+# num = 3
+# i = 1
+
+# while i <= 10:
+#    print(f"{num} x {i} = {num * i}")
+#    i += 1
+
+# count = 1
+
+# while count <= 5:
+#    print(count)
+#    count += 1
+# else:
+#    print("Loop Finished.")   
+
+# fruit = ["apple", "banana", "orange"]
+
+# i = 0
+
+# while i < len(fruit):
+#    print(fruit[i])
+#    i += 1
+
+
+# password = ""
+
+# while password != "python123":
+#    password = input("Enter password:")
+
+# print("Access Granted")   
+
+total = 0
+number = 1
+
+while number != 0:
+   number = int(input("Enter a number (0 to stop):"))
+   total += number
+
+print("Total =", total)   
