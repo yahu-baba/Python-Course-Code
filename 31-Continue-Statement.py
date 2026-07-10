63947bfa98c81140ca6d2075301604ddcef22d54
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-07-10 12:04:01 +0530

    Python Continue & Pass Statement

---
+++ 31-Continue-Statement.py
@@ -0,0 +1,41 @@
+
+# for i in range(1 ,11):
+#    if i == 5:
+#       continue
+
+#    print(i)
+
+
+# for i in range(1 ,11):
+#    if i % 2 == 0:
+#       continue
+   
+#    print(i)
+
+
+# names = ["Rahul", "", "Amit", "", "Priya"]
+
+# for name in names:
+#    if name == "":
+#       continue
+   
+#    print(name)
+
+
+# marks = [75, 35, 88, 29, 91]
+
+# for mark in marks:
+#    if mark < 40:
+#       continue
+   
+#    print(mark)
+
+
+word = "PYTHON"
+
+for letter in word:
+   if letter in "AEIOU":
+      continue
+
+   print(letter)
+
---
+++ 32-Pass-Statement.py
@@ -0,0 +1,15 @@
+# age = 18
+
+# if age >= 18:
+#     pass
+
+# print("Program Continues")
+
+
+numbers = [10, 20, 30]
+
+for num in numbers:
+   if num == 20:
+      pass
+
+   print(num)
