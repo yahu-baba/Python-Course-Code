ed0b084ca7f9df801361b50a6f677f2cce6c8dd6
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-07-15 20:17:58 +0530

    Functions and Functions with Parameters

---
+++ 37-Functions.py
@@ -0,0 +1,15 @@
+
+def greet():
+   print("Welcome to Python!")
+   print("Python is Good Language")
+
+greet() 
+
+print("Hello")
+
+greet()   
+greet() 
+
+print("Hello")
+
+greet()   
---
+++ 38-Functions-Parameters.py
@@ -0,0 +1,25 @@
+
+def hello(fname, lname, age,/ , city):
+   print("Hello", fname, lname, age, city)
+
+hello("YAHU", "BABA", 25, city="Mumbai")   
+# hello(lname = "BABA", fname= "YAHU")   
+# hello("Sanchit", "Soni")   
+
+# def add(a = 1, b = 1):
+#    result = a + b
+#    print("Sum :", result)
+
+# add(10)   
+# add(33, 45)   
+# add(25.25, 45)   
+
+
+# def multiply(a, b):
+#    result = a * b
+#    print("Multiply :", result)
+
+# first = int(input("Enter First Value:"))
+# second =  int(input("Enter Second Value:"))
+
+# multiply(first, second)   
