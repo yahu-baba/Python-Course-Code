0cd87c42eca7d292b900dee89639745cac610d5b
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-07-30 13:28:38 +0530

    String Methods - I

---
+++ 52-String-Methods.py
@@ -0,0 +1,62 @@
+
+# a = "Hello, World!"
+
+# print(a[-5:-2])
+
+# for x in "Python":
+#    print(x)
+
+# print(len(a))
+
+
+
+
+# text = "hello world"
+
+# result = text.capitalize()
+# result = text.title()
+
+# print(result)
+
+
+# text = "python programming language"
+
+# print(text.find("o",5, 7))
+# print(text.rfind("g"))
+
+# print(text.count("p", 5, 10))
+
+# url = "https://google.net"
+
+# print(url.startswith("https"))
+
+# if url.startswith(("https://", "http://")):
+#    print("Valid URL")
+# else:
+#    print("Invalid URL")
+
+# print(url.endswith("com"))
+
+
+# text = "##Python Programming####"
+
+# print(text)
+# print(text.strip("#"))
+# print(text.rstrip("#"))
+
+
+
+# text = "python programming language"
+# text = "Apple-Mango-Banana"
+# text = "Python Java PHP C++ Ruby Go"
+
+# print(text.split(" ", 3))
+
+
+# fruits = ["Apple", "Banana", "Grapes"]
+
+# print("-".join(fruits))
+
+text = "apple apple apple"
+
+print(text.replace("apple", "orange", 2))
