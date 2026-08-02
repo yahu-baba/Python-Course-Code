ed713e8cebea6b03accf798010ba58d932c5513c
YAHUBABA <123031912+yahu-baba@users.noreply.github.com>
2026-08-02 15:37:18 +0530

    Type Casting

---
+++ 54-Type-Casting.py
@@ -0,0 +1,69 @@
+
+# num = 25
+
+# x = float(num)
+
+# print(x)
+# print(type(x))
+
+
+# print(int(float("12.5")))
+
+
+# num = 30.95
+
+# x = str(num)
+
+# print(x)
+# print(type(x))
+
+
+
+# print(bool([1,2]))
+
+
+# name = "Python"
+# number = (10, 20, 30)
+
+# letter = list(number)
+
+# print(letter)
+
+
+# numbers = [10, 20, 20, 30, 30, 30]
+
+# print(set(numbers))
+
+
+# data = [
+#    ("name", "John"),
+#    ("age", 25),
+#    ("city", "Delhi")
+# ]
+
+# print(dict(data))
+
+
+# x = 10
+# result = eval("5 * x")
+
+# print(result)
+
+
+# expression = input("Enter Expression :")
+
+# print(eval(expression))
+
+
+# print(chr(66))
+# print(chr(97))
+# print(chr(1111))
+
+# for i in range(65, 91):
+#    print(chr(i), end=" ")
+
+
+# print(ord("😆"))
+
+
+print(complex(4,3))
