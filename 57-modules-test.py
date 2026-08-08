0737b5d7544bee5b3908986626cfcc784189d284
YOOBABA <123031912+yoo-baba@users.noreply.github.com>
2026-08-08 08:53:13 +0530

    Python Modules & Import Code

---
+++ 57-modules-test.py
@@ -0,0 +1,18 @@
+# import modules as m
+# from modules import greeting, add
+from modules import *
+
+greeting("Sanchit")
+
+print(add(30, 20))
+print(sub(30, 20))
+
+
+# m.greeting("Sanchit")
+
+# print(m.add(30, 20))
+# print(m.sub(30, 20))
+
+# print(m.test)
+
+# print(m.person['name'])
---
+++ modules.py
@@ -0,0 +1,16 @@
+def greeting(name):
+   print("Hello ", name)
+
+def add(a, b):
+   return a + b 
+
+def sub(a, b):
+   return a - b   
+
+test = "Python Course"
+
+person = {
+   "name": "Sanchit",
+   "age": 25,
+   "city": "Chandigarh"
+}
